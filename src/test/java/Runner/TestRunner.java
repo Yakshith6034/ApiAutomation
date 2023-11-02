5956b0bc41f086655441702839465dd30df335fc
Yakshith6034 <korviyakshith@gmail.com>
2023-11-02 10:32:37 +0530

    Added new content to index.html

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,14 +1,16 @@
 package Runner;
 
 import org.junit.runner.RunWith; 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 
 @RunWith (Cucumber.class)
 @CucumberOptions(
-		features = {"src/test/java/features/Get.feature"},
+		features = {"src/test/java/features"},
 		glue = {"stepDefinitions"},
+		plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 		
 		monochrome = true, dryRun = false, publish = true)
 
